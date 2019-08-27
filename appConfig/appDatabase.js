3fe19979e2bd48f76fe3485a25d17b31e973eafb
strotech <37307710+strotech@users.noreply.github.com>
2019-08-27 23:20:10 +0530

    Update appDatabase.js

--- appConfig/appDatabase.js
+++ appConfig/appDatabase.js
@@ -1,5 +1,8 @@
 module.exports={
-    database:'mongodb://localhost:27017/teamDB',
+//    database:'mongodb+srv://cluster0-barww.mongodb.net/portalDB',
+//    mongoUser:'<username not shown here for security reasons>',
+//    mongoPass:'<password not shown here for security reasons>',
+    database:'mongodb://localhost:27017/portalDB',
     usersColl:'team_users',
     activitiesColl:'team_activities',
     secret:'yoursecret'
