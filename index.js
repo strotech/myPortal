cbc3f5160c7d082c31c570ef1611b21514a9a126
strotech <37307710+strotech@users.noreply.github.com>
2019-08-27 23:16:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -80,6 +80,6 @@ app.get('/',function(req,res){
 activityRoutes(app);
 userRoutes(app);
 
-app.listen(3001,function(){
+app.listen(process.env.PORT ||3001,function(){
     console.log('Server started on port 3001......')
 })
